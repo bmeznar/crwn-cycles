d0d5c7ba69941912a018fb2fb1a20b9f30d9c0e6
bmeznar <blaz.meznar.bm@gmail.com>
2024-04-01 18:59:41 +0200

    add sign in component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './routes/navbar/navbar.component';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 import './app.styles.scss';
 
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Navbar />} >
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </ Route>
     </Routes>
   );
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <div>
+      Sign in site
+    </div>
+  )
+}
+
+export default SignIn;
