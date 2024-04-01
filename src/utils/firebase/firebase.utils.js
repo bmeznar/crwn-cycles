38ab96d44668ba13eb784e3dc7e1a485517d43d6
bmeznar <blaz.meznar.bm@gmail.com>
2024-04-01 21:36:05 +0200

    add google sign in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,7 +5,13 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider
-} from 'firebase/auth'
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCeQkEQrb3EGb6gR5I3tfFkrtJmOxkJo2w",
@@ -29,3 +35,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+  // Check if user already exists.
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  // If user doesn't exist already we create a new one.
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return userDocRef;
+};
